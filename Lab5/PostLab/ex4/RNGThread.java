dcf2c6cdf323bbb707955d589cc9795f7054bb24
KaranBengali <karanbengali4@gmail.com>
2017-02-28 16:09:54 -0700

    Updated exercise. Random numbers are now generated using threads

--- Lab5/PostLab/ex4/RNGThread.java
+++ Lab5/PostLab/ex4/RNGThread.java
@@ -3,10 +3,11 @@ import java.util.Random;
 /**
  * Public class RNGThread will generate a random number between 0 and 100
  * @author Karan Bengali
- * @version 2.0
+ * @version 3.0
  * @since February 28, 2017
  */
 public class RNGThread implements Runnable{
+	private int index = 0;
 	public static int rn1;
 	public static int rn2;
 	public static int rn3;
@@ -21,35 +22,37 @@ public class RNGThread implements Runnable{
 	 */
 	private Random r = new Random();
 	/**
-	 * Default constructor, stores a random number in specified index
-	 * of the random number array
-	 * @param i index of the random number array
+	 * Default constructor
 	 */
-	RNGThread(int i){
-		switch(i){
-		case 0:
-			rn1 = r.nextInt(100)+1;
-			break;
-		case 1:
-			rn2 = r.nextInt(100)+1;
-			break;
-		case 2:
-			rn3 = r.nextInt(100)+1;
-			break;
-		case 3:
-			rn4 = r.nextInt(100)+1;
-			break;
-		case 4:
-			rn5 = r.nextInt(100)+1;
-			break;
-		}
-		//rn_array[i] = r.nextInt(100)+1;
+	RNGThread(int index) {
+		this.index = index;
 	}
 	/**
-	 * This method will run the GNR method
+	 * This method will store a random number in an array
 	 */
 	public void run(){
 		try {
+			/*int i = 0;
+			while (rn_array[i] != 0) i++;
+			rn_array[i] = r.nextInt(100)+1;
+			*/
+			switch (index){
+			case 0:
+				rn1 = r.nextInt(100)+1;
+				break;
+			case 1:
+				rn2 = r.nextInt(100)+1;
+				break;
+			case 2:
+				rn3 = r.nextInt(100)+1;
+				break;
+			case 3:
+				rn4 = r.nextInt(100)+1;
+				break;
+			case 4:
+				rn5 = r.nextInt(100)+1;
+				break;
+			}
 			Thread.sleep(100);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -77,7 +80,8 @@ public class RNGThread implements Runnable{
 		}
 		System.out.println(rn1 + " " + rn2 + " " + rn3 + " " + rn4 + " " + rn5 + " " );
 		int total = rn1 + rn2 + rn3 + rn4 + rn5;
-		/*for (int i = 0; i < 5; i++){
+		/*int total = 0;
+		for (int i = 0; i < 5; i++){
 			System.out.print(rn_array[i] + " ");
 			total += rn_array[i];
 		}*/
